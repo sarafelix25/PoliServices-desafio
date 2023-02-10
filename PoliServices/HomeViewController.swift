1db4d1a3b0e3efcbea3341744f7c2ea33bc1b22d
Sara Felix <sfelix@lifescan.com>
2023-02-10 12:19:29 -0300

    criando telas viewcode

---
+++ PoliServices/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  PoliServices
+//
+//  Created by Sara Batista dos Santos Felix on 2/10/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    private lazy var titleLabel: UILabel = {
+
+        return title
+    }()
+}
