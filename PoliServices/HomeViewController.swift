cafc2ea61aaa98bcebe3c1d6cd8974f6dc1fb9cc
Sara Felix <sfelix@lifescan.com>
2023-02-13 20:14:56 -0300

    configurando scene e testando

--- PoliServices/HomeViewController.swift
+++ PoliServices/HomeViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    override func viewDidLoad() {
+        view.backgroundColor = .red
+    }
 //    private lazy var titleLabel: UILabel = {
 //
 //        return title
