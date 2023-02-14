09f75201feb14e65a6f79e5b6562265eba81b684
Sara Felix <sfelix@lifescan.com>
2023-02-13 21:05:12 -0300

    titulo ok, botão em andamento

--- PoliServices/HomeViewController.swift
+++ PoliServices/HomeViewController.swift
@@ -10,10 +10,49 @@ import UIKit
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
-        view.backgroundColor = .red
+        view.backgroundColor = .clear
+        setLayout()
     }
-//    private lazy var titleLabel: UILabel = {
-//
-//        return title
-//    }()
+
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Bem-Vindo ao DevServices"
+        titleLabel.textColor = .white
+        titleLabel.font = .systemFont(ofSize: 32, weight: .bold)
+        titleLabel.numberOfLines = 0
+        return titleLabel
+    }()
+
+
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Solicitar novo serviço", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.backgroundColor = .lightGray
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 15
+        return button
+    }()
+
+    func setLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(button)
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            button.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 14),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -14),
+//            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 20),
+            button.heightAnchor.constraint(equalToConstant: 40)
+
+
+        ])
+    }
 }
