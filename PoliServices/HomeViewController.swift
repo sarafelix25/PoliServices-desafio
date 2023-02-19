0927e859031081ac8c71626510fafa1e55d02bd0
Sara Felix <sfelix@lifescan.com>
2023-02-18 21:13:03 -0300

    titulo e descricao sobre

--- PoliServices/HomeViewController.swift
+++ PoliServices/HomeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
-        view.backgroundColor = UIColor(red: 224, green: 225, blue: 235, alpha: 1.0)
+        view.backgroundColor = .systemGray6
         setLayout()
     }
 
@@ -33,15 +33,53 @@ class HomeViewController: UIViewController {
         return dateLabel
     }()
 
+    private lazy var sobreStackView: UIStackView = {
+        let sobreStackView = UIStackView(arrangedSubviews: [sobreTitleLabel, sobreDescricaoLabel])
+        sobreStackView.translatesAutoresizingMaskIntoConstraints = false
+        sobreStackView.alignment = .fill
+        sobreStackView.axis = .vertical
+        sobreStackView.contentMode = .scaleAspectFill
+        sobreStackView.distribution = .fillProportionally
+        sobreStackView.clipsToBounds = true
+        sobreStackView.autoresizesSubviews = true
+        sobreStackView.layer.cornerRadius = 8
+        sobreStackView.backgroundColor = .white
+        return sobreStackView
+    }()
+
+    private lazy var sobreTitleLabel: UILabel = {
+        let sobreTitleLabel = UILabel()
+        sobreTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        sobreTitleLabel.text = "Sobre nós"
+        sobreTitleLabel.textColor = .black
+        sobreTitleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        sobreTitleLabel.numberOfLines = 0
+        return sobreTitleLabel
+    }()
+
+    private lazy var sobreDescricaoLabel: UILabel = {
+        let sobreDescricaoLabel = UILabel()
+        sobreDescricaoLabel.translatesAutoresizingMaskIntoConstraints = false
+        sobreDescricaoLabel.text = "A DevServices é o melhor aplicativo para reservar seu agendamento com serviços. Aqui é um espaço que você consegue reservar um espaço na minha agenda e vamos resolver suas dúvidas.\nSelecione o tipo de atendimento e vamos pra cima!\n\n*Ilustrativo"
+        sobreDescricaoLabel.textColor = .black
+        sobreDescricaoLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        sobreDescricaoLabel.numberOfLines = 0
+        return sobreDescricaoLabel
+    }()
+
+//    private lazy var line: UILabel = {
+//
+//    }()
+
     private lazy var button: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Solicitar novo serviço", for: .normal)
         button.setTitleColor(.systemBlue, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
-        button.backgroundColor = .systemGray6
+        button.backgroundColor = .systemGray5
         button.clipsToBounds = true
-        button.layer.cornerRadius = 14
+        button.layer.cornerRadius = 16
         return button
     }()
 
@@ -49,18 +87,25 @@ class HomeViewController: UIViewController {
         view.addSubview(titleLabel)
         view.addSubview(dateLabel)
         view.addSubview(button)
+        view.addSubview(sobreStackView)
 
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+
             dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
             dateLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+
+            sobreStackView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 32),
+            sobreStackView.leadingAnchor.constraint(equalTo: dateLabel.leadingAnchor, constant: 32),
+            sobreStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            sobreStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
             button.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             button.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
-            button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 14),
-            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -14),
-//            button.heightAnchor.constraint(equalToConstant: 40)
+            button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
         ])
     }
 }
